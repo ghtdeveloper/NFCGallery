772c428da310756fb704152250b271112fa2b054
ghtdeveloper <martinezfriasedison@gmail.com>
2022-03-29 21:25:39 -0400

    Commit Inicial

---
+++ app/src/main/java/es/usj/mastersa/nfcgallery/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package es.usj.mastersa.nfcgallery.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import es.usj.mastersa.nfcgallery.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/es/usj/mastersa/nfcgallery/view/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package es.usj.mastersa.nfcgallery.view.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import es.usj.mastersa.nfcgallery.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
